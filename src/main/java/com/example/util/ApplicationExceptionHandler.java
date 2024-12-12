6707bd591885493d6dacacad194a72949799c254
Papigalla <ramakullaipapigalla2001@gmail.com>
2024-12-12 11:10:06 +0530

    User Entity,repository,errors

---
+++ src/main/java/com/example/util/ApplicationExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.exception.FailedToUploadException;
+import com.example.exception.UserNotFoundException;
+
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+	@ExceptionHandler
+	public ResponseEntity<ErrorStructure> handleUserNotFound(UserNotFoundException ex) {
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorStructure.create(HttpStatus.NOT_FOUND.value(),
+				ex.getMessage(), "User Not found By the given Id"));
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<ErrorStructure> handleFailedToUpload(FailedToUploadException ex) {
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorStructure.create(HttpStatus.NOT_FOUND.value(),
+				ex.getMessage(), "Failed to upload the Image"));
+	}
+
+}
